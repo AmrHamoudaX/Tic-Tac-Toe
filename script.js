4fa983ce7f232ac91d00507917f16cdf78903045
Amr <legithamouda@gmail.com>
2025-03-09 10:24:17 +0200

    Creates GameBoard and Cell functions
    -Creates the main board and functions to apply mark in the required
    position and show status of game
    -Creates Cell function to apply value = 0 to all squares

---
+++ script.js
@@ -0,0 +1,32 @@
+//GameBoard
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (i = 0; i < rows; i++) {
+    board[i] = [];
+    for (j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+  const getBoard = () => board;
+  const printBoard = () => {
+    return board.map((row) => row.map((cell) => cell.getValue()));
+  };
+  const applyMark = (row, column, player) => {
+    return board[row][column].addMark(player);
+  };
+  return { printBoard, getBoard, applyMark };
+}
+
+function Cell() {
+  let value = 0;
+
+  let addMark = (player) => {
+    value = player;
+  };
+  const getValue = () => value;
+  return { addMark, getValue };
+}
+const board = Gameboard();
